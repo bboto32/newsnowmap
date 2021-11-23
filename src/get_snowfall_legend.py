dca9d965351466e9a6b3392882d06a51439c2ed1
bboto32 <bboto32@gmail.com>
2021-11-23 22:13:04 +0000

    make colorscale colors.png available for images_to-volume

--- src/get_snowfall_legend.py
+++ src/get_snowfall_legend.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('./images/data.1hrain.png')
+img = cv2.imread('./static/data.1hrain.png')
 
 img_cr = img[12:30, 52:892, :]
 
@@ -10,5 +10,5 @@ color_len = img_cr.shape[1] // n_color
 img_sample = img_cr[8:9, 20::40, :]
 colors_legend = [.1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 50, 65, 80, 95, 110]
 
-cv2.imwrite('./images/colors.png', img_sample)
+cv2.imwrite('./static/colors.png', img_sample)
 
