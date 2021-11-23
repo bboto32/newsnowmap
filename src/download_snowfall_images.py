e9054cf4b390e7d26c68c3557ced9bf905692d3d
bboto32 <bboto32@gmail.com>
2021-11-23 21:35:09 +0100

    download works also when nothing is downloaded yet

--- src/download_snowfall_images.py
+++ src/download_snowfall_images.py
@@ -5,16 +5,23 @@ import requests
 import shutil
 from datetime import datetime, timedelta
 import os
+import pytz
 
-os.chdir('/home/boto/snowfall')
+os.chdir('/home/boto/newsnowmap')
 
-with open('./data/downloaded', 'r') as f:
-    for line in f:
-        pass
-    last = line.strip()
+utc_now = pytz.utc.localize(datetime.utcnow())
+cet_now = utc_now.astimezone(pytz.timezone("Europe/Prague"))
 
-last_img_dt = datetime.strptime(last, '%Y%m%d.%H%M')
-current_dt = datetime.now() - timedelta(hours=1) # UTC time
+try:
+    with open('./data/downloaded', 'r') as f:
+        for line in f:
+            pass
+        last = line.strip()
+except FileNotFoundError:
+    last = datetime.strftime(utc_now - timedelta(days=3), '%Y%m%d.%H00')
+
+last_img_dt = pytz.timezone("UTC").localize(datetime.strptime(last, '%Y%m%d.%H%M'))
+current_dt = utc_now
 
 while current_dt > last_img_dt:
     new_img_dt = last_img_dt + timedelta(hours=1)
@@ -31,4 +38,4 @@ while current_dt > last_img_dt:
             f.write(new_dt+'\n')
         last_img_dt = new_img_dt
 
-print('Download done!', datetime.now())
+print('Download done!', datetime.utcnow(), 'UTC')
